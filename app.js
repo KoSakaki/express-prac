311ddfa494f583b20bcb11deff95b05e8c1e6ab8
KoSakaki <sakaki@flaginc.net>
2022-07-05 21:34:24 +0900

    type:ESM

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-var express = require('express');
+// var express = require('express');
+import express from 'express';
 var app = express();
 
 app.get("/", (req, res) => {
+    console.log("listen");
     res.status(200).send("Hello World");
 });
 app.listen(3000);
